768a99fb5f5f55cddd1dccc87b47eabbcffbf7d6
Trevor Reed <mktrevor@gmail.com>
2025-01-14 11:24:38 -0500

    Initial connection hook and worker integration

--- hyrex/__init__.py
+++ hyrex/__init__.py
@@ -6,6 +6,7 @@ from .hyrex_context import HyrexContext, get_hyrex_context
 from .hyrex_queue import HyrexQueue
 from .hyrex_registry import HyrexRegistry
 from .worker.worker import HyrexWorker
+from .connection_hook import ConnectionHook
 
 # Set up null handler at library root level
 logging.getLogger("hyrex").addHandler(logging.NullHandler())
---
+++ hyrex/connection_hook.py
@@ -0,0 +1,40 @@
+from typing import Dict, Any
+from abc import ABC, abstractmethod
+
+
+class ConnectionHook(ABC):
+    """Base class for connection hooks"""
+
+    def __init__(self):
+        self._connection = None
+
+    @property
+    def name(self) -> str:
+        """Unique identifier for this connection type"""
+        return self.__class__.__name__
+
+    @abstractmethod
+    def create_connection(self) -> Any:
+        """Create a new connection"""
+        pass
+
+    @abstractmethod
+    def close_connection(self, connection: Any) -> None:
+        """Close the given connection"""
+        pass
+
+    def get_connection(self) -> Any:
+        """Get the cached connection, creating it if necessary"""
+        if self._connection is None:
+            self._connection = self.create_connection()
+        return self._connection
+
+    def cleanup(self) -> None:
+        """Internal method to clean up the current connection"""
+        if self._connection is not None:
+            self.close_connection(self._connection)
+            self._connection = None
+
+    def health_check(self) -> bool:
+        """Verify the connection is still viable"""
+        return True
