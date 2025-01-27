4831f5b747c1fa30aaa968c2cba27530cccd5165
Trevor Reed <mktrevor@gmail.com>
2025-01-27 16:19:42 -0500

    Update executor table

--- hyrex/sql/sql.py
+++ hyrex/sql/sql.py
@@ -95,30 +95,28 @@ CREATE TABLE IF NOT EXISTS hyrex_task_result
 """
 
 CREATE_HYREX_EXECUTOR_TABLE = """
-    CREATE TABLE IF NOT EXISTS hyrex_executor
-    (
-        id             UUID    NOT NULL PRIMARY KEY,
-        name           VARCHAR NOT NULL,
-        worker_name    VARCHAR NOT NULL,
-        queue_pattern  VARCHAR NOT NULL,
-        queues         JSON    NOT NULL,
-        started        TIMESTAMP WITH TIME ZONE,
-        stopped        TIMESTAMP WITH TIME ZONE,
-        last_heartbeat TIMESTAMP WITH TIME ZONE,
-        stats          JSON
-    );
+DO $$
+    BEGIN
+        -- Create enum if it doesn't exist
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'executor_status') THEN
+            CREATE TYPE executor_status AS ENUM ('SHUTDOWN', 'LOST', 'RUNNING', 'UNKNOWN');
+        END IF;
+END$$;
 
-    DROP VIEW IF EXISTS hyrex_executor_with_status;
-
-    CREATE VIEW hyrex_executor_with_status AS
-    SELECT *,
-           CASE
-               WHEN stopped IS NOT NULL AND started IS NOT NULL THEN 'SHUTDOWN'
-               WHEN last_heartbeat < NOW() - INTERVAL '5 minutes' THEN 'LOST'
-               WHEN started IS NOT NULL THEN 'RUNNING'
-               ELSE 'UNKNOWN'
-               END AS status
-    FROM hyrex_executor;
+-- Create or replace the table with the status column
+CREATE TABLE IF NOT EXISTS hyrex_executor
+(
+    id             UUID    NOT NULL PRIMARY KEY,
+    name           VARCHAR NOT NULL,
+    worker_name    VARCHAR NOT NULL,
+    queue_pattern  VARCHAR NOT NULL,
+    queues         JSON    NOT NULL,
+    started        TIMESTAMP WITH TIME ZONE,
+    stopped        TIMESTAMP WITH TIME ZONE,
+    last_heartbeat TIMESTAMP WITH TIME ZONE,
+    stats          JSON,
+    status         executor_status NOT NULL DEFAULT 'UNKNOWN'
+);
 """
 
 FETCH_TASK = """
