537acd7560a8c174b558069c2b235b215990af74
Trevor Reed <mktrevor@gmail.com>
2025-02-14 11:11:57 -0500

    Consolidate postgres dispatchers

--- hyrex/dispatcher/postgres_dispatcher.py
+++ hyrex/dispatcher/postgres_dispatcher.py
@@ -30,7 +30,7 @@ class PostgresDispatcher(Dispatcher):
         self.pool = ConnectionPool(
             conn_string + "?keepalives=1&keepalives_idle=60&keepalives_interval=10",
             open=True,
-            max_idle=300,
+            max_idle=5,
         )
 
         self.local_queue = Queue()
--- hyrex/dispatcher/postgres_lite_dispatcher.py
+++ hyrex/dispatcher/postgres_lite_dispatcher.py
@@ -11,111 +11,25 @@ from uuid_extensions import uuid7
 from hyrex import constants
 from hyrex.dispatcher.dispatcher import (
     DequeuedTask,
-    Dispatcher,
     EnqueueTaskRequest,
     TaskStatus,
 )
+from hyrex.dispatcher.postgres_dispatcher import PostgresDispatcher
 from hyrex.hyrex_queue import HyrexQueue
 from hyrex.sql import sql
 
 
 # Single-threaded variant of Postgres dispatcher. (Slower enqueuing.)
-class PostgresLiteDispatcher(Dispatcher):
+class PostgresLiteDispatcher(PostgresDispatcher):
     def __init__(self, conn_string: str):
         self.conn_string = conn_string
         self.pool = ConnectionPool(
             conn_string + "?keepalives=1&keepalives_idle=60&keepalives_interval=10",
             open=True,
-            max_idle=300,
+            max_idle=5,
         )
 
         self.register_shutdown_handlers()
-
-    @contextmanager
-    def transaction(self):
-        with self.pool.connection() as conn:
-            with RawCursor(conn) as cur:
-                try:
-                    yield cur
-                except InterruptedError:
-                    conn.rollback()
-                    raise
-            conn.commit()
-
-    def register_app(self, app_info: dict):
-        with self.transaction() as cur:
-            cur.execute(sql.REGISTER_APP_INFO_SQL, [1, app_info])
-
-    def mark_success(self, task_id: UUID):
-        with self.transaction() as cur:
-            cur.execute(sql.MARK_TASK_SUCCESS, [task_id])
-
-    def mark_failed(self, task_id: UUID):
-        with self.transaction() as cur:
-            cur.execute(sql.MARK_TASK_FAILED, [task_id])
-
-    def attempt_retry(self, task_id: UUID):
-        with self.transaction() as cur:
-            cur.execute(sql.CONDITIONALLY_RETRY_TASK, [task_id, uuid7()])
-
-    def try_to_cancel_task(self, task_id: UUID):
-        with self.transaction() as cur:
-            cur.execute(sql.TRY_TO_CANCEL_TASK, [task_id])
-
-    def task_canceled(self, task_id: UUID):
-        with self.transaction() as cur:
-            cur.execute(sql.TASK_CANCELED, [task_id])
-
-    def dequeue(
-        self,
-        executor_id: UUID,
-        queue: str = constants.ANY_QUEUE,
-        concurrency_limit=0,
-    ) -> DequeuedTask:
-        dequeued_task = None
-        with self.transaction() as cur:
-            if queue == constants.ANY_QUEUE:
-                cur.execute(sql.FETCH_TASK_FROM_ANY_QUEUE, [executor_id])
-            else:
-                if concurrency_limit > 0:
-                    cur.execute(
-                        sql.FETCH_TASK_WITH_CONCURRENCY,
-                        [queue, concurrency_limit, executor_id],
-                    )
-                else:
-                    cur.execute(sql.FETCH_TASK, [queue, executor_id])
-            row = cur.fetchone()
-            if row:
-                (
-                    task_id,
-                    durable_id,
-                    root_id,
-                    parent_id,
-                    task_name,
-                    args,
-                    queue,
-                    priority,
-                    timeout_seconds,
-                    scheduled_start,
-                    queued,
-                    started,
-                ) = row
-                dequeued_task = DequeuedTask(
-                    id=task_id,
-                    durable_id=durable_id,
-                    root_id=root_id,
-                    parent_id=parent_id,
-                    task_name=task_name,
-                    args=args,
-                    queue=queue,
-                    priority=priority,
-                    timeout_seconds=timeout_seconds,
-                    scheduled_start=scheduled_start,
-                    queued=queued,
-                    started=started,
-                )
-
-        return dequeued_task
 
     def enqueue(
         self,
@@ -147,63 +61,3 @@ class PostgresLiteDispatcher(Dispatcher):
         self.logger.debug("Stopping dispatcher...")
         self.pool.close()
         self.logger.debug("Dispatcher stopped successfully!")
-
-    def get_task_status(self, task_id: UUID) -> TaskStatus:
-        with self.transaction() as cur:
-            cur.execute(sql.GET_TASK_STATUS, [task_id])
-            result = cur.fetchone()
-            if result is None:
-                raise ValueError(f"Task id {task_id} not found in DB.")
-            return result[0]
-
-    def register_executor(
-        self,
-        executor_id: UUID,
-        executor_name: str,
-        queue_pattern: str,
-        queues: list[HyrexQueue],
-        worker_name: str,
-    ):
-        with self.transaction() as cur:
-            cur.execute(
-                sql.REGISTER_EXECUTOR,
-                [executor_id, executor_name, queue_pattern, queues, worker_name],
-            )
-
-    def disconnect_executor(self, executor_id: UUID):
-        with self.transaction() as cur:
-            cur.execute(sql.DISCONNECT_EXECUTOR, [executor_id])
-
-    def executor_heartbeat(self, executor_ids: list[UUID], timestamp: datetime):
-        with self.transaction() as cur:
-            cur.execute(sql.EXECUTOR_HEARTBEAT, [timestamp, executor_ids])
-
-    def update_executor_stats(self, executor_id: UUID, stats: dict):
-        with self.transaction() as cur:
-            cur.execute(sql.UPDATE_EXECUTOR_STATS, [executor_id, stats])
-
-    def task_heartbeat(self, task_ids: list[UUID], timestamp: datetime):
-        with self.transaction() as cur:
-            cur.execute(sql.TASK_HEARTBEAT, [timestamp, task_ids])
-
-    def get_tasks_up_for_cancel(self) -> list[UUID]:
-        with self.transaction() as cur:
-            cur.execute(sql.GET_TASKS_UP_FOR_CANCEL)
-            return [row[0] for row in cur.fetchall()]
-
-    def mark_running_tasks_lost(self, executor_id: UUID):
-        with self.transaction() as cur:
-            cur.execute(sql.MARK_RUNNING_TASKS_LOST, [executor_id])
-
-    def save_result(self, task_id: UUID, result: str):
-        with self.transaction() as cur:
-            cur.execute(sql.SAVE_RESULT, [task_id, result])
-
-    def get_queues_for_pattern(self, pattern: str) -> list[str]:
-        with self.transaction() as cur:
-            cur.execute(sql.GET_QUEUES_FOR_PATTERN, [pattern])
-            return [row[0] for row in cur.fetchall()]
-
-    def register_task(self, task_name: str, cron: str = None, source_code: str = None):
-        with self.transaction() as cur:
-            cur.execute(sql.UPSERT_TASK, [task_name, cron, source_code])
