ddedd41269eae8729a12436e374870c21f7c377a
Trevor Reed <mktrevor@gmail.com>
2025-03-03 15:22:40 -0500

    Shift task run methods to durable run

--- hyrex/durable_run.py
+++ hyrex/durable_run.py
@@ -1,22 +1,32 @@
 import logging
+import time
 from uuid import UUID
 
 from pydantic import BaseModel
 
 from hyrex.dispatcher.dispatcher import Dispatcher
+from hyrex.schemas import TaskStatus
 
 
 class TaskRun(BaseModel):
+    task_run_id: UUID
+    status: TaskStatus
+    result: dict
+
+
+class DurableTaskRun:
     def __init__(
         self,
         task_name: str,
-        task_run_id: str,
+        durable_id: UUID,
         dispatcher: Dispatcher,
     ):
-
+        self.logger = logging.getLogger(__name__)
         self.task_name = task_name
-        self.task_run_id = task_run_id
+        self.durable_id = durable_id
         self.dispatcher = dispatcher
+
+        self.task_runs = list[TaskRun]
 
     def wait(self, timeout: float = 30.0, interval: float = 1.0):
         start = time.time()
@@ -34,30 +44,17 @@ class TaskRun(BaseModel):
             task_status = self.dispatcher.get_task_status(task_id=self.task_run_id)
             elapsed = time.time() - start
 
-    # TODO: Implement
     def get_result(self):
-        return self.dispatcher.get_result(self.task_run_id)
+        # TODO: Find successful task run first.
+        task_run_id = None
+        return self.dispatcher.get_result(task_run_id)
 
     def cancel(self):
+        # TODO: Find currently active task.
         self.dispatcher.try_to_cancel_task(self.task_run_id)
 
     def __repr__(self):
-        return f"TaskRun<{self.task_name}>[{self.task_run_id}]"
-
-
-class DurableTaskRun:
-    def __init__(
-        self,
-        task_name: str,
-        durable_id: UUID,
-        dispatcher: Dispatcher,
-    ):
-        self.logger = logging.getLogger(__name__)
-        self.task_name = task_name
-        self.durable_id = durable_id
-        self.dispatcher = dispatcher
-
-        self.task_runs = []
+        return f"DurableTaskRun<{self.task_name}>[{self.durable_id}]"
 
     def refresh(self):
         # TODO
